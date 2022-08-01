0ae7b24f6e2899779b733eaafdf6d2ddc39eed1a
Namik <namazov306@gmail.com>
2022-08-01 19:26:01 +0300

    Create Network Manager, create network call in network manager to follower list controller, add enum for different error messages using utilities and result type, change title color of get followers button (don't want white color text, my bad)

---
+++ githubFollowersApp/githubFollowersApp/Sources/Modules/Persistence/NetworkManager.swift
@@ -0,0 +1,57 @@
+//
+//  NetworkManager.swift
+//  githubFollowersApp
+//
+//  Created by Намик on 8/1/22.
+//
+
+import Foundation
+
+final class NetworkManager {
+    static let shared = NetworkManager()
+    
+    let baseURL = "https://api.github.com/users/"
+    
+    private init() {}
+    
+    func getFollowers(for username: String,
+                      page: Int,
+                      completion: @escaping (Result<[FollowerModel],
+                                             GetFollowersErrorMessage>) -> Void) {
+        let endpoint = baseURL + "\(username)/followers?per_page=100&page=\(page)"
+        
+        guard let url = URL(string: endpoint) else {
+            completion(.failure(.invalidUsername))
+            return
+        }
+        
+        let task = URLSession.shared.dataTask(with: url) { data, response, error in
+            if let _ = error {
+                completion(.failure(.unableToComplete))
+                return
+            }
+            
+            guard let response = response as? HTTPURLResponse,
+                  response.statusCode == 200 else {
+                completion(.failure(.invalidResponse))
+                return
+            }
+            
+            guard let data = data else {
+                completion(.failure(.invalidData))
+                return
+            }
+            
+            do {
+                let decoder = JSONDecoder()
+                decoder.keyDecodingStrategy = .convertFromSnakeCase
+                let followers = try decoder.decode([FollowerModel].self,
+                                                   from: data)
+                completion(.success(followers))
+            } catch {
+                completion(.failure(.invalidData))
+            }
+        }
+        task.resume()
+    }
+}
---
+++ githubFollowersApp/githubFollowersApp/Sources/Modules/Utilities/GetFollowersErrorMessage.swift
@@ -0,0 +1,19 @@
+//
+//  ErrorMessage.swift
+//  githubFollowersApp
+//
+//  Created by Намик on 8/1/22.
+//
+
+import Foundation
+
+enum GetFollowersErrorMessage: String, Error {
+    case invalidUsername = "This username created an invalid request. Please try again."
+    case unableToComplete = "Unable to complete your request. Please check your internet connection."
+    case invalidResponse = "Invalid response from the server. Please try again."
+    case invalidData = "The data received from the server was invalid. Please try again."
+}
+
+
+
+
