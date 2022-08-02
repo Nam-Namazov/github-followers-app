ac16624e2149eed2e12fae67d631c0d60ea250bc
Namik <namazov306@gmail.com>
2022-08-02 20:45:03 +0300

    Name refactoring, create and add header view to FollowerUserInfoViewController, with 2 labels name and login, profile image, location label and icon and bio label, fetch data from server and add to the header

--- githubFollowersApp/githubFollowersApp/Sources/Modules/FollowerList/Model/FollowerModel.swift
+++ githubFollowersApp/githubFollowersApp/Sources/Modules/FollowerList/Model/FollowerModel.swift
@@ -10,8 +10,4 @@ import Foundation
 struct FollowerModel: Codable, Hashable {
     var login: String
     var avatarUrl: String
-    
-    func hash(into hasher: inout Hasher) {
-        hasher.combine(login)
-    }
 }
